f8da8d7b193e482d833f0fd75ac0b7ef5ecb69cc
JeyM <jeymson8000@gmail.com>
2025-09-10 18:48:23 +0800

    dev-branch-2 start

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../fetch-connections/account-connection";
-
+//
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
